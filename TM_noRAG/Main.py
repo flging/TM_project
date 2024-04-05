c203ed7431cf77b3e768bf5a7dc79facb45689b1
Kimoften <ivan1007@naver.com>
2024-04-05 18:04:53 +0900

    .

--- TM_noRAG/Main.py
+++ TM_noRAG/Main.py
@@ -1,6 +1,4 @@
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse
-from fastapi.middleware.cors import CORSMiddleware
+from flask import Flask, request, jsonify
 from TM_find_page import find_gri_pages
 from TM_agent_getindex import get_index
 from TM_extract_text import extract_text_from_pages
@@ -8,47 +6,59 @@ from TM_agent import get_draft
 from Indextranslate import translate
 import json
 
-app = FastAPI()
-
-# CORS 미들웨어 추가
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # 모든 도메인에서의 요청 허용
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+app = Flask(__name__)
 
 pdf_path = "TM_noRAG/2023 Integrated Report_Kor.pdf"  # 전기보고서 첨부 경로
 
-@app.post("/index_list")
-async def show_index_list(raw_data: str):
+
+@app.route('/create_draft', methods=['POST'])
+def create_draft():
+    raw_data = request.json['raw_data']
+    selected_numbers = request.json['selected_numbers']
+
+    index_list = show_index_list(raw_data)
+    draft = create_draft_from_raw_data(raw_data, index_list, selected_numbers, pdf_path)
+
+    return jsonify({"draft": draft})
+
+
+def show_index_list(raw_data):
     index_list = json.loads(get_index(raw_data))
     return index_list
 
-@app.post("/create_draft")
-async def create_draft(raw_data: str, selected_numbers: list):
-    index_list = json.loads(get_index(raw_data))
+
+def create_draft_from_raw_data(raw_data, index_list, selected_numbers, pdf_path):
     draft = []
     for number in selected_numbers:
         disclosure_num = index_list[number]['disclosure_num']
         pages = find_gri_pages(pdf_path, disclosure_num)
-        if isinstance(pages, list):
+        if type(pages) == list:
             extracted_pages = extract_text_from_pages(pdf_path, pages)
         else:
             extracted_pages = ["no page in previous report"]
         for extracted_page in extracted_pages:
-            small_draft = {"pages": pages, "disclosure_num": disclosure_num}
-            small_draft["draft"] = get_draft(extracted_page, disclosure_num, raw_data)
-            draft.append(small_draft)
+            small_draft = [pages, disclosure_num]
+            small_draft.append(get_draft(extracted_page, disclosure_num, raw_data))
+        draft.append(small_draft)
     return draft
 
-@app.post("/get_gri_title")
-async def get_gri_title(raw_data: str):
-    index_list = json.loads(get_index(raw_data))
-    title_list = [translate(index['disclosure_num']) for index in index_list]
+
+@app.route('/get_gri_titles', methods=['POST'])
+def get_gri_titles():
+    raw_data = request.json['raw_data']
+    index_list = show_index_list(raw_data)
+    title_list = get_gri_title(index_list)
+    return jsonify({"titles": title_list})
+
+
+def get_gri_title(index_list):
+    title_list = []
+    for index in index_list:
+        gri = index['disclosure_num']
+        gri_title = translate(gri)
+        title_list.append(gri_title)
     return title_list
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+if __name__ == '__main__':
+    app.run(debug=True)
