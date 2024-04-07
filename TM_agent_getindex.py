4a8938df607a8516055ddc96114a06176b1a971b
Kimoften <ivan1007@naver.com>
2024-04-07 14:49:42 +0900

    .

--- TM_agent_getindex.py
+++ TM_agent_getindex.py
@@ -5,15 +5,21 @@ def get_index(raw_data):
     response = client.chat.completions.create(
     model="gpt-4-turbo-preview",
     messages=[
-        {"role": "system", "content": """ ESG보고서를 작성하기 위해 GRI에 따라 데이터 가공을 할거야.  
-         다음 기준 맞춰서 데이터 가공해줘.
-         1. 반드시 데이터에 가장 적합한 5개의 disclosure_number를 정해.
-         2. 각 disclosure_number마다 데이터와 관련해서 해당 번호를 고른 이유 설명해. (description)
-         3. disclosure_number는 반드시 숫자-숫자의 형식으로 자세히 정해야 해. (GRI Number-number)의 형식으로 반드시 'GRI'가 숫자 앞에 붙어야 해. 
-         4. disclosure_number의 Number는 ##반드시## 다음 범위 안의 숫자여야 해: 200~400 또는 1~3.##100번대 숫자는 포함하지 않아야 해.
-         5. 다음과 같은 형식으로만 응답을 생성해:
-         [{"disclosure_num": "GRI {Number-number}", "description": description }, {"disclosure_num": "GRI {Number-number}", "description": description },...].
-    """},
+        {"role": "system", "content": """ You are a GRI Standards expert. Based on your best understanding of all indicators within the GRI Standards, please identify five GRI Standards indexes to which ***company interview content*** applies. 
+
+[form of answer].
+For each GRI Index, briefly state the key content of the GRI Index in **one sentence**, followed by the key reason for presenting the GRI Index in **one sentence**.
+
+[Notes]
+1) Do not include **GRI Indexes numbered in the 100s** in your answer as they are not applicable.
+2) For more specific application, treat all Index numbers like example below and address the detailed guideline content.
+@@example
+GRI 403-3: 직업 건강 및 안전 관리 시스템
+- 조직의 직업 건강 및 안전 관리 시스템에 대한 정보를 제공합니다.
+- 안전 조회 번역 웹 구축은 직원들의 안전과 건강을 위한 정보 접근성을 높이는 조치로, 직업 건강 및 안전 관리 시스템의 일환으로 볼 수 있습니다.
+@@
+
+Try your best and **Answer in Korean**."""},
         {"role": "user", "content": f"raw_data:{raw_data}"}
     ],
     temperature=0
