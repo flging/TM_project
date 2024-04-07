5f6af72c7d3bce451e9b2f2cda6ef56d383c3d3b
Kimoften <ivan1007@naver.com>
2024-04-07 16:13:38 +0900

    .

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ async def upload_pdf(pdf_file: UploadFile = File(...)):
     return {"pdf_path": temp_pdf_path}
 
 @app.post("/enter_raw_data/")
-async def enter_raw_data(raw_data: List[str] = Form(...)):
+async def enter_raw_data(raw_data: UploadFile = File(...)):
     return {"raw_data": raw_data}
 
 @app.post("/enter_raw_data_info/")
